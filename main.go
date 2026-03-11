453f5b01a31f75293cd993ae1e259d35e3b1c50c
grrbrhrrrurrghhha-cpu <grrbrhrrrurrghhha@gmail.com>
2026-03-11 17:05:19 +0500

    update main.go

--- main.go
+++ main.go
@@ -120,7 +120,7 @@ func ExecuteBrainfuck(code string, input string) string {
   return output
 }
 
-func ExecutePercentCaretAnd78(code string, input string) string {
+func ExecutePercentCaretAnd78(code string) string {
   var output string
   var grid = [256][256]bool{}
   var index, ops int
