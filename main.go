f7facf5e8c604b48a6daa2ff5f330616f40b78cb
grrbrhrrrurrghhha-cpu <grrbrhrrrurrghhha@gmail.com>
2026-03-13 19:09:34 +0500

    oops

--- main.go
+++ main.go
@@ -573,7 +573,7 @@ func ExecuteBefunge93(code string, input string) string {
         }
       case '&':
         n := 0
-        for index < len(input) && input[index] == ' ' && input[index] == '\n' && input[index] == '\r' {
+        for index < len(input) || input[index] == ' ' || input[index] == '\n' || input[index] == '\r' {
           index++
         }
         for index < len(input) && input[index] >= '0' && input[index] <= '9' {
